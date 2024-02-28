5cc1d618d6b32fe6c1d836af41d4e0d38514fa4e
InnocentWahome <innocentwahome@gmail.com>
2024-02-28 04:26:42 +0300

    add tabs to profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,14 +1,53 @@
 <script setup lang="ts">
-
 definePageMeta({
-  layout: 'default',
-})
+  layout: "default",
+});
 
+const activeKey = ref("1");
 </script>
 
 
 <template>
   <div>
-    Profile
+    <!-- ---------------------------------------------- -->
+    <!-- Header -->
+    <!-- ---------------------------------------------- -->
+    <a-breadcrumb style="height: 40px; display: flex; align-items: center">
+      <a-breadcrumb-item style="color: #5f8524; font-weight: 600"
+        >Dashboard</a-breadcrumb-item
+      >
+      <a-breadcrumb-item>{{ pageNameCapitalized }}</a-breadcrumb-item>
+    </a-breadcrumb>
+
+    <a-space direction="vertical" style="width: 100%">
+      <a-row
+        :gutter="[
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+        ]"
+      >
+        <a-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+          <a-page-header
+            title="Profile"
+            sub-title="Manage your account"
+            :ghost="false"
+          >
+          </a-page-header>
+        </a-col>
+      </a-row>
+    </a-space>
+
+    <!-- ---------------------------------------------- -->
+    <!-- Tabs -->
+    <!-- ---------------------------------------------- -->
+    <a-tabs v-model:activeKey="activeKey">
+      <a-tab-pane key="1" tab="Profile details">
+        <FormsUserForm />
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Your reports" force-render
+        >Content of Tab Pane 2</a-tab-pane
+      >
+      <a-tab-pane key="3" tab="Reset your password">Content of Tab Pane </a-tab-pane>
+    </a-tabs>
   </div>
 </template>
--- pages/updates.vue
+++ pages/updates.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
-
 definePageMeta({
-  layout: 'default',
-})
-
+  layout: "default",
+});
 </script>
 
 
 <template>
   <div>
-    Updates
+    <!-- ---------------------------------------------- -->
+    <!-- Header -->
+    <!-- ---------------------------------------------- -->
+    <a-breadcrumb style="height: 40px; display: flex; align-items: center">
+      <a-breadcrumb-item style="color: #5f8524; font-weight: 600"
+        >Dashboard</a-breadcrumb-item
+      >
+      <a-breadcrumb-item>{{ pageNameCapitalized }}</a-breadcrumb-item>
+    </a-breadcrumb>
+
+    <a-space direction="vertical" style="width: 100%">
+      <a-row
+        :gutter="[
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+          { xs: 8, sm: 16, md: 24, lg: 32 },
+        ]"
+      >
+        <a-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+          <a-page-header
+            title="Updates"
+            sub-title="Latest reports and cases in Nairobi"
+            :ghost="false"
+          >
+          </a-page-header>
+        </a-col>
+      </a-row>
+    </a-space>
   </div>
 </template>
